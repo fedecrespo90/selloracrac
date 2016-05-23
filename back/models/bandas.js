74fa657774e375f36258e3d0d62ca912f4539476
fedecrespo90 <fedecrespo90@gmail.com>
2016-05-23 07:49:49 -0300

    Modifying model

--- back/models/bandas.js
+++ back/models/bandas.js
@@ -1,9 +1,8 @@
-
 var Schema = require('mongoose').Schema
  
 var banda_schema = new Schema({
   name        :   String,
-  link        :   String
+  bio         :   String
 });
 
 module.exports = banda_schema;
