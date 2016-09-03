7d06dc5ee1bc000214f3613d8a0ccd4d47d9a283
Martín <martinpalmieri@gmail.com>
2016-09-03 20:35:34 -0300

    New view and controller for band profile

---
+++ app/public/js/controllers/BandProfileController.js
@@ -0,0 +1,15 @@
+function bandas ($scope, bandas) {
+	var selectedBand = $scope.$route.current.pathParams.name;
+	$scope.$parent.mainBackground = '';
+  bandas.success(function(data){
+    $scope.bandas = data;
+    $scope.banda = $scope.bandas.filter(function(el) {
+    	return el.name === selectedBand;
+    });
+    $scope.banda = $scope.banda[0];
+  });
+}
+
+angular
+	.module('Site')
+	.controller('BandProfileController', ['$scope', 'bandas', bandas]);
