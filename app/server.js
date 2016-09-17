bda96e0c8118b05195b4f3671fa103b11915d18e
Fede Crespo <fedecrespo90@gmail.com>
2016-09-17 20:02:41 -0300

    Updating visitors query

--- app/server.js
+++ app/server.js
@@ -38,7 +38,7 @@ app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/favicon', function(req, res){
-	stats.saveVisitor(req, res, count);
+	stats.saveVisitor(req, res, counter);
 	res.send(200);
 });
 var api = require('./routes/banda')(app,express);
